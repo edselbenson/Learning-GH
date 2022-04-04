fc9a380720a1162dfaf636b906d6cdc63d395961
Edsel Benson <edselbenson@yahoo.com>
2022-04-04 21:34:36 +1000

    Initial Commit

---
+++ Learning_GH/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Learning_GH
+//
+//  Created by Edsel Benson on 4/4/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Learning_GH/Learning_GHApp.swift
@@ -0,0 +1,17 @@
+//
+//  Learning_GHApp.swift
+//  Learning_GH
+//
+//  Created by Edsel Benson on 4/4/2022.
+//
+
+import SwiftUI
+
+@main
+struct Learning_GHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
